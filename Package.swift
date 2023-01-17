19dfa63e80c91b10bc7852bc3da7d3a754fc26eb
Nikolai Timonin <nikki.timonin@yandex.ru>
2023-01-17 23:06:20 +0600

    Add swiftlint

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "PagingList",
-            targets: ["PagingList"]),
+            targets: ["PagingList"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,6 +20,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PagingList",
-            dependencies: []),
+            dependencies: [])
     ]
 )
